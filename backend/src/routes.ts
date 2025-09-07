3cd2c90a5c497145e15b5c570138b8fb053d82cd
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-09-07 15:08:17 -0300

    feat: adicionando rotas com fastify FitScore, Buscar FitScores Login e Cadastro

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { CreateUserController } from "./controllers/CreateUserController";
 import { DeleteUserController } from "./controllers/DeleteUserController";
 import { ListUserController } from "./controllers/ListUserController";
 import { UpdateUserController } from "./controllers/UpdateUserController";
+import prismaClient from "./prisma";
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
 
@@ -15,9 +16,87 @@ export async function routes(fastify: FastifyInstance, options: FastifyPluginOpt
         await new ListUserController().handle(request, reply)
     })
 
+    // CADASTRO
     fastify.post("/users", async (request: FastifyRequest, reply: FastifyReply) => {
 
         await new CreateUserController().handle(request, reply)
+    })
+
+    // LOGIN
+    fastify.post("/login", async (request: FastifyRequest, reply: FastifyReply) => {
+
+        const { email, password } = request.body as { email: string; password: string };
+
+        try {
+            const user = await prismaClient.user.findFirst({ where: { email } });
+
+            if (!user) {
+                return reply.status(404).send({ error: "Usuário não encontrado" });
+            }
+
+            if (user.password !== password) {
+                return reply.status(401).send({ error: "Senha incorreta" });
+            }
+
+            // Retornarnado Somente os Dados que sera enviado para o FRONT-END
+            const userForFront = {
+                id: user.id,
+                name: user.name,
+                email: user.email
+            };
+
+            return reply.send(userForFront);
+
+        } catch (error) {
+            return reply.status(500).send({ error: "Erro interno do servidor" });
+        }
+    })
+
+    // FITSCORE
+    fastify.post("/fitscore", async (request: FastifyRequest, reply: FastifyReply) => {
+
+        const { userId, score, classificacao, respostas } = request.body as {
+
+            userId: string,
+            score: number,
+            classificacao: string,
+            respostas: any[]
+        }
+
+        try {
+
+            const newFit = await prismaClient.fitScore.create({
+
+                data: { userId, score, classificacao, respostas }
+            })
+
+            return reply.send(newFit)
+        } catch (error) {
+            return reply.status(500).send({ error: "Erro ao salvar FitScore" })
+        }
+    })
+
+    // BUSCAR FITSCORES
+    fastify.get("/fitscores/:userId", async (request: FastifyRequest, reply: FastifyReply) => {
+
+        const { userId } = request.params as { userId: string }
+
+        try {
+
+            const fitscores = await prismaClient.fitScore.findMany({
+                where: { userId },
+                include: {
+                    user: {
+                        select: { id: true, name: true, email: true }
+                    }
+                }
+            })
+
+            return reply.send(fitscores)
+        } catch (err) {
+            console.log(err)
+            return reply.status(500).send({ error: "Erro ao buscas FitScores" })
+        }
     })
 
     fastify.delete("/users/:id", async (request: FastifyRequest, reply: FastifyReply) => {
