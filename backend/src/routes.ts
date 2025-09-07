158765de99bfac1992eaf869cb2d2ed65723b9ff
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-09-06 23:36:19 -0300

    feat: New routes put, delete

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,14 +1,32 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } from "fastify";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { DeleteUserController } from "./controllers/DeleteUserController";
+import { ListUserController } from "./controllers/ListUserController";
+import { UpdateUserController } from "./controllers/UpdateUserController";
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
 
-    fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
+    fastify.get("/test", async (request: FastifyRequest, reply: FastifyReply) => {
         return { ok: true }
+    })
+
+    fastify.get("/users", async (request: FastifyRequest, reply: FastifyReply) => {
+
+        await new ListUserController().handle(request, reply)
     })
 
     fastify.post("/users", async (request: FastifyRequest, reply: FastifyReply) => {
 
         await new CreateUserController().handle(request, reply)
+    })
+
+    fastify.delete("/users/:id", async (request: FastifyRequest, reply: FastifyReply) => {
+
+        await new DeleteUserController().handle(request, reply)
+    })
+
+    fastify.put("/users/:id", async (request: FastifyRequest, reply: FastifyReply) => {
+
+        await new UpdateUserController().handle(request, reply)
     })
 }
