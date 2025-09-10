da7cc5b4493c1acd50a4a7de03720af7dc66969d
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-09-09 21:34:10 -0300

    feat: trocando request.body para params e fazendo comentarios

--- backend/src/controllers/DeleteUserController.ts
+++ backend/src/controllers/DeleteUserController.ts
@@ -5,13 +5,27 @@ class DeleteUserController {
 
     async handle(request: FastifyRequest, reply: FastifyReply) {
 
-        const { id } = request.query as { id: string }
+        const { id } = request.params as { id: string }
+
+        // request.params: São os Parâmetros da URL
+        // request.bod: Conteúdo Enviado no corpo da Requisição POST/PUT
 
         const userService = new DeleteUser()
 
-        const user = await userService.execute({ id })
 
-        reply.send(user)
+        try {
+            const user = await userService.execute({ id })
+
+            return reply.status(200).send({
+                message: 'Usuário deletado com sucesso.',
+                user
+            })
+        } catch (error) {
+            return reply.status(400).send({
+                message: 'Erro ao deletar usuário',
+                error: (error as Error).message
+            })
+        }
     }
 }
 
